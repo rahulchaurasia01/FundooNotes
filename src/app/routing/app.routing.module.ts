53f8dcfb323accfd23000801051c482ab23ff3e3
Rahul Chaurasia <rahulchaurasia08@gmail.com>
2020-02-12 17:56:09 +0530

    Routing for login, registration, forgetPassword, reset Password.

--- src/app/routing/app.routing.module.ts
+++ src/app/routing/app.routing.module.ts
@@ -4,13 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../components/login/login.component';
 import { SignupComponent } from '../components/signup/signup.component';
 import { ForgetpasswordComponent } from '../components/forgetpassword/forgetpassword.component';
+import { ResetpasswordComponent } from '../components/resetpassword/resetpassword.component';
 
 const routes: Routes = [
 
-  { path: '', component: LoginComponent },
+  { path: '',  component: LoginComponent },
   { path: 'login', component: LoginComponent},
   { path: 'signup', component: SignupComponent },
-  { path: 'forgetpassword', component: ForgetpasswordComponent }
+  { path: 'forgetpassword', component: ForgetpasswordComponent },
+  { path: 'resetpassword', redirectTo: "" },
+  { path: 'resetpassword/:token', component: ResetpasswordComponent }
 
 ]
 

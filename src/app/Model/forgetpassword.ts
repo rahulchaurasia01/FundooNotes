fee2bfec1cfa85e74c0f30d3005cab3f113b38a6
Rahul Chaurasia <rahulchaurasia08@gmail.com>
2020-02-12 17:55:07 +0530

    Completed Forget Password Component.

---
+++ src/app/Model/forgetpassword.ts
@@ -0,0 +1,5 @@
+export interface Forgetpassword {
+
+    emailid: string;
+
+}
--- src/app/components/forgetpassword/forgetpassword.component.ts
+++ src/app/components/forgetpassword/forgetpassword.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import { FormGroup, Validators, FormControl } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { UserService } from '../../services/user.service';
+import { Forgetpassword } from 'src/app/Model/forgetpassword';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-forgetpassword',
@@ -12,7 +14,7 @@ export class ForgetpasswordComponent implements OnInit {
 
   forgetPasswordInformation: FormGroup;
 
-  constructor(private http: HttpClient) { }
+  constructor(private user: UserService, private _snackBar: MatSnackBar) { }
 
   ngOnInit() {
 
@@ -26,5 +28,28 @@ export class ForgetpasswordComponent implements OnInit {
     return this.forgetPasswordInformation.controls[controlName].hasError(errorName);
   }
 
+  forgetPassword(forgetPasswordData) {
+    if(this.forgetPasswordInformation.valid)
+      this.sendDataToServer(forgetPasswordData);
+  }
+
+  private sendDataToServer(forgetPasswordData) {
+    var forgetPassword: Forgetpassword = {
+      emailid: forgetPasswordData.email
+    }
+
+    this.user.forgetPassword(forgetPassword)
+      .subscribe(data => {
+        this._snackBar.open(data.message, "Close", {
+          duration: 3000,
+        });
+      },
+      (error => {
+        this._snackBar.open(error.error.message, "Close", {
+          duration: 3000,
+        });
+      }))
+
+  }
 
 }

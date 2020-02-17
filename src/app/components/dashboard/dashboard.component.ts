1803e586fadd65c70d35b53b227eb28eeb132dee
Rahul Chaurasia <rahulchaurasia08@gmail.com>
2020-02-17 19:31:31 +0530

    Work Still Going On DashBoard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, OnDestroy } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LabelsService } from '../../services/labels.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,9 +12,40 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private _router: Router) { }
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
+
+  labels: any;
+  token: string;
+  title: string = "Keep";
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private _router: Router,
+    private label: LabelsService, private _snackBar: MatSnackBar) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
   ngOnInit() {
+    this.token = localStorage.getItem("fundooToken");
+    this.GetAllLabels(this.token);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+
+  GetAllLabels(token) : any {
+    this.label.GetAllLabels(token).
+      subscribe(data => {
+        this.labels = data.data; 
+      },
+      error => {
+        this._snackBar.open(error.error.message, "Close", {
+          duration: 3000,
+        });
+      })
   }
 
   onFileInput(event) : void {
@@ -21,7 +55,6 @@ export class DashboardComponent implements OnInit {
   doLogout(): void {
     localStorage.removeItem("fundooToken")
     this._router.navigate(['login']);
-    
   }
 
 }

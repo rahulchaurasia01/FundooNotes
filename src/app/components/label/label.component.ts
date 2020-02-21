d37db555d9282517a37406de46da199d0ecdfec9
Rahul Chaurasia <rahulchaurasia08@gmail.com>
2020-02-21 19:29:14 +0530

    Added Services of label

--- src/app/components/label/label.component.ts
+++ src/app/components/label/label.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+import { LabelsService } from '../../services/labels.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-label',
   templateUrl: './label.component.html',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LabelComponent implements OnInit {
 
-  constructor() { }
+  labelId: number;
+  chckError: string;
+  labelNotes=[];
+  labelIcon: string;
+  emptyLabelText: string;
+
+  constructor(private routeParam: ActivatedRoute, private label: LabelsService, private _snackBar: MatSnackBar) { }
 
   ngOnInit() {
+
+    this.labelId = this.routeParam.snapshot.params['id'];
+    this.labelIcon = "label";
+    this.emptyLabelText = "No notes with this label yet";
+    this.GetNotesByLabelId();
+
+  }
+
+
+  GetNotesByLabelId() {
+    this.label.GetNotesByLabelId(this.labelId).
+      subscribe(data => {
+        if(data.status) 
+          this.labelNotes = data.data;
+      },
+      error => {
+
+        if(error.error.message)
+            this.chckError = error.error.message;
+          else
+            this.chckError= "Connection to the Server Failed";
+
+        this._snackBar.open(this.chckError, "Close", {
+          duration: 3000,
+        });
+
+      })
   }
 
 }

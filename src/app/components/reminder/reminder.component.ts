2e33c4a40dc8fd3c096e4a58a7a7d017ff1d0444
Rahul Chaurasia <rahulchaurasia08@gmail.com>
2020-02-21 19:30:00 +0530

    Added Reminder Services

--- src/app/components/reminder/reminder.component.ts
+++ src/app/components/reminder/reminder.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NotesService } from '../../services/notes.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-reminder',
   templateUrl: './reminder.component.html',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReminderComponent implements OnInit {
 
-  constructor() { }
+  chckError: string;
+  reminderNotes=[];
+  reminderIcon: string;
+  emptyReminderText: string;
+
+  constructor(private notes: NotesService, private _snackBar: MatSnackBar) { }
 
   ngOnInit() {
+
+    this.reminderIcon = "notification_important";
+    this.emptyReminderText = "Notes with upcoming reminders appear here";
+    this.GetAllReminderNotes();
+
+  }
+
+
+  GetAllReminderNotes() {
+    this.notes.GetAllReminderNotes().
+      subscribe(data => {
+        if(data.status)
+          this.reminderNotes = data.data;
+      },
+      error => {
+
+        if(error.error.message)
+          this.chckError = error.error.message;
+        else
+          this.chckError= "Connection to the Server Failed";
+
+        this._snackBar.open(this.chckError, "Close", {
+          duration: 3000,
+        });
+      })
   }
 
 }

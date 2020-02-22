a81fe2bda5c3578e108d8eee00fb40e76d9a5082
Rahul Chaurasia <rahulchaurasia08@gmail.com>
2020-02-22 18:51:34 +0530

    create services for archive, delete, bulk delete and restore

--- src/app/services/notes.service.ts
+++ src/app/services/notes.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { HttpServiceService } from '../services/http-service.service';
 import { HttpHeaders } from '@angular/common/http';
+import { Archivenote } from '../Model/archivenote';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class NotesService {
 
   constructor(private http: HttpServiceService) { }
 
-  createHttpOptions(token: string) : any {
+  private createHttpOptions(token: string) : any {
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type':  'application/json',
@@ -46,5 +47,25 @@ export class NotesService {
     return this.http.get("Notes/Delete", true, httpOptions);
   }
 
+  archiveTheNote(noteId: number, archiveNote: Archivenote) {
+    const httpOptions = this.createHttpOptions(localStorage.getItem("fundooToken"));
+    return this.http.put("Notes/" + noteId + "/Archive", archiveNote, true, httpOptions);
+  }
+
+  deleteNote(noteId: number) {
+    const httpOptions = this.createHttpOptions(localStorage.getItem("fundooToken"));
+    return this.http.delete("Notes/"+noteId, true, httpOptions);
+  }
+
+  bulkDeleteNote() {
+    const httpOptions = this.createHttpOptions(localStorage.getItem("fundooToken"));
+    return this.http.delete("Notes/BulkDelete", true, httpOptions);
+  }
+
+  restoreTheNote(noteId: number) {
+    const httpOptions = this.createHttpOptions(localStorage.getItem("fundooToken"));
+    console.log(httpOptions);
+    return this.http.put("Notes/Restore/"+noteId, true, httpOptions);
+  }
 
 }

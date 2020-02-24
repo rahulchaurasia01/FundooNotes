aa34711f03bfd0325ac558fa367d28afb638f79c
Rahul Chaurasia <rahulchaurasia08@gmail.com>
2020-02-24 19:22:01 +0530

    Shifted Icons View and Functionality to Icons Component

--- src/app/components/displaynote/displaynote.component.ts
+++ src/app/components/displaynote/displaynote.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { NotesService } from '../../services/notes.service';
-import { Archivenote } from 'src/app/Model/archivenote';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { DeletedialogComponent } from '../deletedialog/deletedialog.component';
+import { Pinnote } from 'src/app/Model/pinnote';
 
 @Component({
   selector: 'app-displaynote',
@@ -28,10 +28,49 @@ export class DisplaynoteComponent implements OnInit {
 
   }
 
-  singleDeleteNote(noteId: number) {
-    this.deleteText = "Delete note forever?";
-    this.deleteButtonText = "Delete";
-    this.openSingleDeleteDialog(noteId);
+  recieveDataFromIconChild($event: any) {
+    this.displayNotes = this.displayNotes.filter(note => note.noteId !== $event);
+  }
+
+  pinTheNote(noteId: number, flag: boolean) {
+
+    var pinNote: Pinnote = {
+      IsPin: flag
+    }
+
+    this.note.pinTheNote(noteId, pinNote).
+      subscribe(data => {
+        if(!data.status) {
+          if(flag) 
+            this.infoMsg = "Unable to Pin the Note";
+          else
+            this.infoMsg = "Unable to UnPin the Note";
+          this._snackBar.open(this.infoMsg, "Close", {
+            duration: 5000,
+          });
+        }
+        else {
+          this.displayNotes.forEach(element => {
+            if(element.noteId == noteId) {
+              element.isPin = flag
+              if(this.parentIcon == "archive") {
+                element.isArchived = data.data.isArchived;
+                this.displayNotes = this.displayNotes.filter(note => note.noteId !== noteId);
+              }
+            }
+          }); 
+        }
+      }, 
+      error => {
+        if(error.error.message)
+            this.chckError = error.error.message;
+          else
+            this.chckError= "Connection to the Server Failed";
+
+        this._snackBar.open(this.chckError, "Close", {
+          duration: 3000,
+        });
+      })
   }
 
   bulkDelete() {
@@ -40,32 +79,11 @@ export class DisplaynoteComponent implements OnInit {
     this.openBulkDeleteDialog()
   }
 
-  openSingleDeleteDialog(noteId: number): void {
-    const dialogRef = this.dialog.open(DeletedialogComponent, {
-      data:  {deleteText: this.deleteText,
-         deleteButtonText: this.deleteButtonText,
-         noteId: noteId},
-      
-         panelClass: 'editLabelDialogContainer',
-         width: '430px'
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if(result) {
-        this.displayNotes = this.displayNotes.filter(note => note.noteId !== noteId);
-      }
-      else {
-        this._snackBar.open("Unable to delete the Note", "Close", {
-          duration: 5000,
-        });
-      }
-    });
-  }
-
   openBulkDeleteDialog(): void {
     const dialogRef = this.dialog.open(DeletedialogComponent, {
       data:  {deleteText: this.deleteText,
-         deleteButtonText: this.deleteButtonText},
+         deleteButtonText: this.deleteButtonText,
+         noOfNotes: this.displayNotes.length},
 
          panelClass: 'editLabelDialogContainer',
          width: '430px'
@@ -81,61 +99,6 @@ export class DisplaynoteComponent implements OnInit {
         });
       }
     });
-  }
-
-  archiveUnarchiveTheNote(noteId: number, flag: boolean) {
-
-    var archiveNote: Archivenote = {
-      IsArchive: flag
-    };
-
-    this.note.archiveTheNote(noteId, archiveNote).
-      subscribe(data => {
-        if(data.status) {
-          this.displayNotes = this.displayNotes.filter(noted => noted.noteId !== noteId);
-        
-        if(flag)
-          this.infoMsg = "Note archived";
-        else
-          this.infoMsg = "Note unarchived";
-        this._snackBar.open(this.infoMsg, "Close", {
-          duration: 5000,
-        });
-        }
-      }, 
-      error => {
-        if(error.error.message)
-            this.chckError = error.error.message;
-          else
-            this.chckError= "Connection to the Server Failed";
-
-        this._snackBar.open(this.chckError, "Close", {
-          duration: 3000,
-        });
-      })
-  }
-
-  restoreNote(noteId: number) {
-    this.note.restoreTheNote(noteId).
-      subscribe(data => {
-        if(data.status) {
-          this.displayNotes = this.displayNotes.filter(noted => noted.noteId !== noteId);
-
-          this._snackBar.open("Note restored", "Close", {
-            duration: 5000,
-          });
-        }
-      }, 
-      error => {
-        if(error.error.message)
-            this.chckError = error.error.message;
-          else
-            this.chckError= "Connection to the Server Failed";
-
-        this._snackBar.open(this.chckError, "Close", {
-          duration: 3000,
-        });
-      })
   }
 
 }

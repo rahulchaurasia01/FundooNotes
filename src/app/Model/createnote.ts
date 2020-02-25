a8ad961a593e043ec9be1b8016728143dcc46e26
Rahul Chaurasia <rahulchaurasia08@gmail.com>
2020-02-25 19:21:22 +0530

    Created a Model For Creating a note

---
+++ src/app/Model/createnote.ts
@@ -0,0 +1,14 @@
+export interface Createnote {
+
+    Title: string;
+    Description: string;
+    Color: string;
+    Image: string;
+    IsPin: boolean;
+    IsArchived: boolean;
+    IsDeleted: boolean;
+    Reminder: string;
+    Label:any
+    Collaborators: any;
+
+}

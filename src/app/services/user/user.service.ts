4ffecfbcee1ae28bdfcdcf5af292d6781d4124bd
Rahul Chaurasia <rahulchaurasia08@gmail.com>
2020-02-29 18:48:09 +0530

    Added Get All user Api

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -6,6 +6,7 @@ import { Signup } from '../../Model/signup';
 import { Forgetpassword } from '../../Model/forgetpassword';
 import { Resetpassword } from '../../Model/resetpassword';
 import { HttpHeaders } from '@angular/common/http';
+import { Alluser } from 'src/app/Model/alluser';
 
 @Injectable({
   providedIn: 'root'
@@ -26,8 +27,12 @@ export class UserService {
     return this.httpService.post("User/ForgetPassword", forgetPasswordInformation);
   }
 
-  resetPassword(resetPasswordInformation: Resetpassword, token: string) {
-    return this.httpService.post("User/ResetPassword", resetPasswordInformation, true);
+  resetPassword(resetPasswordInformation: Resetpassword) {
+    return this.httpService.postResetPassword("User/ResetPassword", resetPasswordInformation, true);
+  }
+
+  GetAllUser(userInitial: Alluser) {
+    return this.httpService.post("User/Users", userInitial, true);
   }
 
 

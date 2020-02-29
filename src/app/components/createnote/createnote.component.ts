bb955b27f905471fb55661f27b4ded293dc42b23
Rahul Chaurasia <rahulchaurasia08@gmail.com>
2020-02-29 18:47:07 +0530

    Add Note Icon Component In Create Note Component

--- src/app/components/createnote/createnote.component.ts
+++ src/app/components/createnote/createnote.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '
 import { Createnote } from 'src/app/Model/createnote';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { NotesService } from '../../services/note/notes.service';
+import { UserService } from '../../services/user/user.service';
+import { Alluser } from 'src/app/Model/alluser';
+import { Collaborator } from 'src/app/Model/collaborator';
 
 @Component({
   selector: 'app-createnote',
@@ -18,66 +21,34 @@ export class CreatenoteComponent implements OnInit {
   isPinned: boolean = false;
   isArchive: boolean = false;
   collaboratorClicked: boolean = true;
-  userName: string;
-  userEmail: string;
-  newCollaborator: string;
+  noteIconAccessFrom: string;
   collaboratorUserList=[];
-  tempCollaboratorUserList=[];
+
 
   @Output()
   noteCreated = new EventEmitter<any>();
 
   @ViewChild("collab") collabFocus: ElementRef;
 
-  constructor(private note: NotesService, private _snackBar: MatSnackBar) { }
+  constructor(private note: NotesService, private _snackBar: MatSnackBar, private _user: UserService) { }
 
   ngOnInit() {
     this.showTakeANoteAndAction = true;
     this.hideNoteWithImagesDiv = true;
-    this.userName = localStorage.getItem("fundooUserName");
-    this.userEmail = localStorage.getItem("fundooUserEmail");
+    this.noteIconAccessFrom = "Create Note";
   }
 
   sendMessageToParent(note: any) {
     this.noteCreated.emit(note);
   }
 
+  updateCollabToCreateNote($event: any) {
+    this.collaboratorUserList = $event;
+  }
+
   NoteClick() {
     this.showTakeANoteAndAction = false;
     this.hideNoteWithImagesDiv = false;
-  }
-
-  collaboratorClickedByUser() {
-    this.collaboratorClicked = false;
-    this.tempCollaboratorUserList = this.collaboratorUserList;
-  }
-
-  cancelCollaboratorButtonClick() {
-    console.log(this.tempCollaboratorUserList.length);
-    this.tempCollaboratorUserList = [];
-    console.log(this.tempCollaboratorUserList.length);
-    this.newCollaborator = '';
-    this.collaboratorClicked = true;
-    console.log(this.collaboratorUserList.length);
-  }
-
-  addCollaboratorToArray() {
-    this.tempCollaboratorUserList.push(this.newCollaborator);
-    this.newCollaborator = '';
-    this.collabFocus.nativeElement.focus();
-  }
-
-  saveCollaboratorButtonClick() {
-    console.log("hola");
-    this.collaboratorUserList = this.tempCollaboratorUserList;
-    this.newCollaborator = '';
-    this.collaboratorClicked = true;
-  }
-
-  
-
-  deleteCollaboratedUser(user: string) {
-    this.tempCollaboratorUserList = this.tempCollaboratorUserList.filter(users => users !== user);
   }
 
   userPinnedTheNote(flag: boolean) {
@@ -91,9 +62,11 @@ export class CreatenoteComponent implements OnInit {
   closeButtonClick() {
     this.showTakeANoteAndAction = true;
     this.hideNoteWithImagesDiv = true;
-    if((this.createNoteDesciption != null && this.createNoteDesciption != '') || 
-            (this.createNoteTitle != null && this.createNoteTitle != ''))
-    {
+
+    var collaborator=[];
+
+    if ((this.createNoteDesciption != null && this.createNoteDesciption != '') ||
+      (this.createNoteTitle != null && this.createNoteTitle != '')) {
       var createNote: Createnote = {
         Title: this.createNoteTitle,
         Description: this.createNoteDesciption,
@@ -104,28 +77,28 @@ export class CreatenoteComponent implements OnInit {
         IsDeleted: false,
         Reminder: '',
         Label: null,
-        Collaborators: null
+        Collaborators: collaborator
       };
 
       this.note.createNote(createNote).
         subscribe(data => {
-          if(data.status) {
+          if (data.status) {
             this.sendMessageToParent(data.data);
           }
         },
-        error => {
-          if(error.error.message)
-            this.chckError = error.error.message;
-          else
-            this.chckError= "Connection to the Server Failed";
+          error => {
+            if (error.error.message)
+              this.chckError = error.error.message;
+            else
+              this.chckError = "Connection to the Server Failed";
 
-        this._snackBar.open(this.chckError, "Close", {
-          duration: 3000,
-        });
-        })  
+            this._snackBar.open(this.chckError, "Close", {
+              duration: 3000,
+            });
+          })
 
     }
-    this.createNoteDesciption ='';
+    this.createNoteDesciption = '';
     this.createNoteTitle = '';
     this.isPinned = false;
     this.isArchive = false;

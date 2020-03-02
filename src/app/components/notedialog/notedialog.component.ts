b23a0d2bf7e5a9224ebbb41511423c7e310e8be5
Rahul Chaurasia <rahulchaurasia08@gmail.com>
2020-03-02 19:33:45 +0530

    Worked On Images, color etc

--- src/app/components/notedialog/notedialog.component.ts
+++ src/app/components/notedialog/notedialog.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Inject } from '@angular/core';
+
+import { NotesService } from '../../services/note/notes.service';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Pinnote } from 'src/app/Model/pinnote';
+import { Updatenote } from 'src/app/Model/updatenote';
 
 @Component({
   selector: 'app-notedialog',
@@ -8,35 +13,104 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class NotedialogComponent implements OnInit {
 
+  chckError: string;
   note: any;
-  isPinned: boolean;
-  isArchive: boolean;
   createNoteTitle: string;
   createNoteDesciption: string;
 
-  constructor(public dialogRef: MatDialogRef<NotedialogComponent>, 
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+  AccessingFrom: string;
+
+  constructor(public dialogRef: MatDialogRef<NotedialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any, private notes: NotesService,
+    private _snackBar: MatSnackBar) { }
 
   ngOnInit() {
 
+    this.AccessingFrom = "Edit Note";
+
     this.note = this.data.note;
 
-    this.isPinned = this.note.isPin;
     this.createNoteTitle = this.note.title;
     this.createNoteDesciption = this.note.description;
-    this.isArchive = this.note.isArchived;
   }
 
-  userPinnedTheNote(flag: boolean) {
-    this.isPinned = flag;
+  updateNoteInEditNote($event) {
+    this.note = $event;
   }
 
-  userArchiveTheNote(flag: boolean) {
-    this.isArchive = flag;
+  updateArchive($event) {
+    this.note = $event;
+    this.dialogRef.close(this.note);
+  }
+
+  userPinnedTheNote(noteId: number, flag: boolean) {
+
+
+    var pinTheNote: Pinnote = {
+      IsPin: flag
+    }
+
+    this.notes.pinTheNote(noteId, pinTheNote).
+      subscribe(data => {
+        if (data.status) {
+          this.note.isPin = flag;
+        }
+        else {
+          this._snackBar.open(data.message, "Close", {
+            duration: 3000,
+          });
+        }
+      },
+        error => {
+          if (error.error.message)
+            this.chckError = error.error.message;
+          else
+            this.chckError = "Connection to the Server Failed";
+
+          this._snackBar.open(this.chckError, "Close", {
+            duration: 3000,
+          });
+        })
+
   }
 
   closeButtonClick() {
-    this.dialogRef.close();
+
+    if ((this.createNoteTitle != this.note.title) || (this.createNoteDesciption != this.note.description)) {
+
+      this.note.title = this.createNoteTitle;
+      this.note.description = this.createNoteDesciption
+
+      var updateNote: Updatenote = {
+        Title: this.note.title,
+        Description: this.note.description,
+        Reminder: this.note.Reminder,
+        Label: this.note.labels
+      }
+
+      this.notes.updateNote(this.note.noteId, updateNote).
+        subscribe(data => {
+          if(!data.status) {
+            this._snackBar.open(this.chckError, "Close", {
+              duration: 3000,
+            });
+          }
+        },
+        error => {
+          if (error.error.message)
+            this.chckError = error.error.message;
+          else
+            this.chckError = "Connection to the Server Failed";
+
+          this._snackBar.open(this.chckError, "Close", {
+            duration: 3000,
+          });
+        })
+
+
+    }
+
+    this.dialogRef.close(this.note);
   }
 
 }

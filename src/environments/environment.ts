5c8beb9fe1d1e6c6c5eec89a3ff776dd3c0f2f48
Rahul Chaurasia <rahulchaurasia08@gmail.com>
2020-03-17 18:55:42 +0530

    Added Firebase connection property.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,18 @@
 export const environment = {
   production: false,
 
-  BaseUrl: 'http://localhost:49798/api/'
+  BaseUrl: 'http://localhost:49798/api/',
+
+  firebase: {
+    apiKey: "AIzaSyBUdNtjfgmh-N-yOWx_kuF8dbdzN_7a1Aw",
+    authDomain: "fundoonotes-1580121043579.firebaseapp.com",
+    databaseURL: "https://fundoonotes-1580121043579.firebaseio.com",
+    projectId: "fundoonotes-1580121043579",
+    storageBucket: "fundoonotes-1580121043579.appspot.com",
+    messagingSenderId: "670336277527",
+    appId: "1:670336277527:web:65c5dead7c2187ccae9052",
+    measurementId: "G-CDEEVQX2ER"
+  }
 
 };
 

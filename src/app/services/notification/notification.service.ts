551757d0279bdf1b52d86e74cd1475e4aaf2b244
Rahul Chaurasia <rahulchaurasia92@hotmail.com>
2020-03-21 15:10:47 +0530

    Notification service to contact with firebase

--- src/app/services/notification/notification.service.ts
+++ src/app/services/notification/notification.service.ts
@@ -2,18 +2,23 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFireMessaging } from '@angular/fire/messaging';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from "../user/user.service";
 import { take } from 'rxjs/operators';
 import { BehaviorSubject } from 'rxjs';
+import { Notification } from 'src/app/Model/notification';
 
 @Injectable()
 export class NotificationService {
 
+  chckError: string;
   currentMessage = new BehaviorSubject(null);
 
   constructor(private angularFireDB: AngularFireDatabase, private angularFireAuth: AngularFireAuth,
-    private angularFireMessaging: AngularFireMessaging) {
-try {
-  
+    private angularFireMessaging: AngularFireMessaging, private _snackBar: MatSnackBar,
+    private user: UserService) {
+    try {
+
 
       this.angularFireMessaging.messaging.subscribe(
         (_messaging) => {
@@ -22,16 +27,44 @@ try {
         }
       )
     } catch (error) {
-  console.log('error in catch ',error);
-  
+      console.log('error in catch ', error);
+
     }
   }
 
   requestPermission() {
     this.angularFireMessaging.requestToken.subscribe(
       (token) => {
-        console.log(token);
-        // this.updateToken(token);
+
+        if (localStorage.getItem("fundooNotification") == null) {
+
+          localStorage.setItem("fundooNotification", token);
+
+          var notificationToken: Notification = {
+            Token: token
+          };
+
+          this.user.notification(notificationToken).
+            subscribe(data => {
+              if (!data.status) {
+                this._snackBar.open(data.message, "Close", {
+                  duration: 5000,
+                });
+              }
+            },
+              error => {
+                if (error.error.message)
+                  this.chckError = error.error.message;
+                else
+                  this.chckError = "Connection to the Server Failed";
+
+                this._snackBar.open(this.chckError, "Close", {
+                  duration: 3000,
+                });
+              }
+            )
+        }
+
       },
       error => {
         console.error("Unable to get permission to notify.", error);
